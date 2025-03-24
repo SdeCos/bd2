52db5e368310fe14b5659adbb47254d5ce16a19f
SdeCos <saul.decossanchez@alumnos.uneatlantico.es>
2025-03-24 13:18:44 +0100

    clase 24 marzo

--- mongodb/ejercicios/plantilla/orm/ConnectionSettings.php
+++ mongodb/ejercicios/plantilla/orm/ConnectionSettings.php
@@ -2,7 +2,7 @@
 
 return [
   'host' => '127.0.0.1:3308',
-  'db' => 'uni_2025', //sakila || uni_2025
+  'db' => 'uni_2025', //sakila || uni_2025 || mongodb_objects
   'user' => 'saul',
   'password' => '1234',
 ];
